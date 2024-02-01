d0a477539b1b0a0a7a0400319b56a4bbdd661b36
Stosh09 <stoshochieng@gmail.com>
2024-02-01 12:55:38 +0300

    Date functionality allowing website to display current date and time

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const country = document.getElementById('country');
 const weatherForecastElement = document.getElementById('weather-forecast');
 const currentTempElement = document.getElementById('current-temp')
 
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
 setInterval(() => {
 	const time = new Date();
 	const month = time.getMonth();
@@ -13,9 +17,10 @@ setInterval(() => {
 	const day = time.getDay();
 	const hour = time.getHours();
 	const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
-	const minutes = time.getMinutes().toString().padStart(2, '0');;
-	console.log(`${minutes}`)
+	const minutes = time.getMinutes().toString().padStart(2, '0');
 	const ampm = hour >= 12 ? 'PM' : 'AM'
 
 	timeElement.innerHTML = hoursIn12HrFormat + ':' + minutes + `<span id="am-pm">${ampm}</span>`
+
+	dateElement.innerHTML = days[day] + ', ' + date +' ' + months[month];
 }, 1000);
