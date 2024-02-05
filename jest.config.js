cdbd59455e67a6ec31af3cb7f11668609b4724fe
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2024-02-05 12:36:07 +0300

    add tests

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'jsdom', // Set the test environment to jsdom (for DOM-related tests)
+  setupFiles: ['./geolocationMock.js'], // Add the setup file for geolocation mocking
+  // Add other configuration options if needed
+};
+
